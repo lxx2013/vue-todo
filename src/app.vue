1fa317c5d90c33f66e5c1ab501f5c92ab1509242
lxx.win10 <lxx2013@mail.ustc.edu.cn>
2018-05-18 22:17:04 +0800

    init vue+webpack
    配置看起来挺复杂的,而且所谓的热加载感觉好慢呀

---
+++ src/app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class='test'>{{text}}</div>
+</template>
+
+<script>
+    export default {
+        data() {
+            return {
+                text: 'test 234'
+            }
+        }
+    }
+
+</script>
+<style>
+    .test {
+        color: black;
+    }
+
+</style>
---
+++ webpack.config.js
@@ -0,0 +1,77 @@
+const path = require('path')
+const {
+    VueLoaderPlugin
+} = require("vue-loader")
+const isDev = process.env.NODE_ENV === 'development'
+const HtmlPlugin = require('html-webpack-plugin')
+const webpack = require('webpack')
+
+const config = {
+    target: 'web',
+    entry: path.join(__dirname, 'src/index.js'),
+    output: {
+        filename: 'bundle.js',
+        path: path.join(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                use: ['vue-loader']
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['vue-style-loader', 'css-loader']
+            },
+            {
+                test: /\.less$/,
+                use: [
+                        'vue-style-loader',
+                        'css-loader',
+                        'less-loader'
+                    ]
+            },
+            {
+                test: /\.(gif|jpg|jpeg|png|svg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 1024,
+                        name: '[name].[ext]'
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            'process.env': {
+                NODE_ENV: isDev ? '"development"' : '"production"'
+            }
+        }),
+        new VueLoaderPlugin(),
+        new HtmlPlugin(),
+
+    ]
+
+}
+
+if (isDev) {
+    config.devtool = '#cheap-module-eval-source-map'
+    config.devServer = {
+            port: 8000,
+            host: '0.0.0.0',
+            overlay: {
+                errors: true,
+            },
+            hot: true
+        },
+        config.plugins.push(
+            new webpack.HotModuleReplacementPlugin(), new webpack.NoEmitOnErrorsPlugin()
+        )
+}
+module.exports = config
